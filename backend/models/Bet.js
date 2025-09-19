1bd60ff039ab36dc5b1e1f0a59c860737d7d0902
graby12 <119487566+nelkemboi@users.noreply.github.com>
2025-09-19 19:43:16 +0300

    updates

--- backend/models/Bet.js
+++ backend/models/Bet.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const betSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "Register", required: true },
-    amount: { type: Number, required: true, min: 300 }, // min bet amount
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Register",
+      required: true,
+    },
+    amount: { type: Number, required: true, min: 10 }, // min bet amount
     autoCashOut: { type: Number, default: null }, // optional, must be >= 2 if set
     multiplier: { type: Number, default: null }, // crash multiplier reached
     profit: { type: Number, default: 0 }, // winnings (+) or loss (-)
